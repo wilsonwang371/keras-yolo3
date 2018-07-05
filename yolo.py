f1c0eaa002a8d4a5f9f28a9a50a978fa2f7ec5e4
Wilson Wang <wilsonwang371@users.noreply.github.com>
2018-07-05 11:37:14 -0700

    bug fixing

--- yolo.py
+++ yolo.py
@@ -213,7 +213,10 @@ def detect_img(yolo, arg_img):
             r_image = yolo.detect_image(image)
             cv_r_img = np.array(r_image)
             #r_image.show()
+            cv2.namedWindow('r_image', cv2.WINDOW_NORMAL)
             cv2.imshow('r_image', cv_r_img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
             input("Press Enter to continue...")
         if arg_img != '':
             break
